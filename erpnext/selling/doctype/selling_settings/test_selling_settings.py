d39bf5f72d2078ed29056858d0f5d030348cb83d
shubham-DT <shubham@8848digital.com>
2025-05-02 12:50:37 +0530

    feat: added test cases to cover the test execution of uncovered lines in code_coverage

--- erpnext/selling/doctype/selling_settings/test_selling_settings.py
+++ erpnext/selling/doctype/selling_settings/test_selling_settings.py
@@ -12,3 +12,12 @@ class TestSellingSettings(unittest.TestCase):
 		# if setup was completed correctly
 		default = frappe.db.get_single_value("Selling Settings", "maintain_same_rate_action")
 		self.assertEqual("Stop", default)
+
+	def test_toggle_discount_accounting_fields_coverage_TC_S_187(self):
+		
+		selling_setting = frappe.new_doc("Selling Settings")
+		selling_setting.enable_discount_accounting = 1
+		selling_setting.save()
+		selling_setting.toggle_discount_accounting_fields()
+  
+		self.assertEqual(selling_setting.enable_discount_accounting, 1)
