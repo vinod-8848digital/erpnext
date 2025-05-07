687abcaf5a0899fc93546917942bd0afdc8b284f
vinod-8848digital <vinod@8848digital.com>
2025-05-08 04:47:54 +0530

    fix: validate if from and to time are present on submission of job card

--- erpnext/manufacturing/doctype/job_card/job_card.py
+++ erpnext/manufacturing/doctype/job_card/job_card.py
@@ -706,6 +706,13 @@ class JobCard(Document):
 				)
 			)
 
+		else:
+			for row in self.time_logs:
+				if not row.from_time or not row.to_time:
+					frappe.throw(
+						_("Row #{0}: From Time and To Time fields are required").format(row.idx),
+					)
+
 		precision = self.precision("total_completed_qty")
 		total_completed_qty = flt(
 			flt(self.total_completed_qty, precision) + flt(self.process_loss_qty, precision)
