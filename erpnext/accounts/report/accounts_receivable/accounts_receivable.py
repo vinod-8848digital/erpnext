a936da3c1e1f05b7357ebc5528a85a7a3286f206
vinod-8848digital <vinod@8848digital.com>
2025-05-08 06:40:12 +0530

    refactor: set default for fetch methods

--- erpnext/accounts/report/accounts_receivable/accounts_receivable.py
+++ erpnext/accounts/report/accounts_receivable/accounts_receivable.py
@@ -54,9 +54,10 @@ class ReceivablePayableReport:
 			self.filters.range = "30, 60, 90, 120"
 		self.ranges = [num.strip() for num in self.filters.range.split(",") if num.strip().isdigit()]
 		self.range_numbers = [num for num in range(1, len(self.ranges) + 2)]
-		self.ple_fetch_method = frappe.db.get_single_value(
-			"Accounts Settings", "receivable_payable_fetch_method"
-		)
+		self.ple_fetch_method = (
+			frappe.db.get_single_value("Accounts Settings", "receivable_payable_fetch_method")
+			or "Buffered Cursor"
+		)  # Fail Safe
 
 	def run(self, args):
 		self.filters.update(args)
