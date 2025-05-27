167501783f864168c8abd4553161e0e2c8011197
vinod-8848digital <vinod@8848digital.com>
2025-05-27 12:04:14 +0530

    fix: failed test cases

--- erpnext/selling/report/address_and_contacts/test_address_and_contacts.py
+++ erpnext/selling/report/address_and_contacts/test_address_and_contacts.py
@@ -1,19 +1,20 @@
 import frappe
 from frappe.tests.utils import FrappeTestCase
 
+from erpnext.selling.doctype.customer.test_customer import get_customer_dict
+
+from .address_and_contacts import execute
+
+
 class TestAddressAndContacts(FrappeTestCase):
 	def setUp(self):
 		self.customer = setup_customer_with_address("Test Customer For Address And Contact")
-		self.filters = {
-			"party_type": "Customer",
-			"party_name": self.customer.name
-		}
+		self.filters = {"party_type": "Customer", "party_name": self.customer.name}
 
 	def tearDown(self):
 		frappe.db.rollback()
 
-	def test_address_and_contacts_report(self):
-		from .address_and_contacts import execute
+	def test_address_and_contacts_report_codecov(self):
 		data = execute(self.filters)
 		if data[1]:
 			for row in data[1]:
@@ -30,6 +31,18 @@ class TestAddressAndContacts(FrappeTestCase):
 					self.assertEqual(row[9], "Test Customer For Address And Contact")
 					self.assertEqual(row[10], "Test Last Name")
 
+	def test_customer_without_address_codecov(self):
+		customer = frappe.get_doc(get_customer_dict("_Test_Customer_Without_Address_")).insert(
+			ignore_permissions=True
+		)
+		self.filters.update({"party_name": customer.name})
+		data = execute(self.filters)
+		if data[1]:
+			for row in data[1]:
+				if row[0] == customer.name:
+					self.assertEqual(row[0], "_Test_Customer_Without_Address_")
+
+
 def setup_customer_with_address(customer_name):
 	if not frappe.db.exists("Customer", customer_name):
 		frappe.get_doc(
@@ -38,13 +51,13 @@ def setup_customer_with_address(customer_name):
 				"customer_name": customer_name,
 				"customer_type": "Individual",
 				"territory": "All Territories",
-				"customer_group": "All Customer Groups"
+				"customer_group": "All Customer Groups",
 			}
 		).insert(ignore_permissions=True)
 
 		frappe.get_doc(
 			{
-				"doctype":"Address",
+				"doctype": "Address",
 				"address_title": customer_name,
 				"address_type": "Billing",
 				"address_line1": "Hyderabad",
@@ -54,12 +67,7 @@ def setup_customer_with_address(customer_name):
 				"country": "India",
 				"pincode": "500075",
 				"is_primary_address": 1,
-				"links": [
-					{
-						"link_doctype": "Customer",
-						"link_name": customer_name
-					}
-				]
+				"links": [{"link_doctype": "Customer", "link_name": customer_name}],
 			}
 		).insert(ignore_permissions=True)
 
@@ -70,27 +78,11 @@ def setup_customer_with_address(customer_name):
 				"middle_name": "Test Middle Name",
 				"last_name": "Test Last Name",
 				"status": "Passive",
-				"address": customer_name+"-Billing",
+				"address": customer_name + "-Billing",
 				"is_primary_contact": 1,
-				"email_ids": [
-					{
-						"email_id": "abcdtest@gmail.com",
-						"is_primary": 1
-					}
-				],
-				"phone_nos": [
-					{
-						"phone": "7897895874",
-						"is_primary_phone": 1,
-						"is_primary_mobile_no": 1
-					}
-				],
-				"links": [
-					{
-						"link_doctype": "Customer",
-						"link_name": customer_name
-					}
-				]
+				"email_ids": [{"email_id": "abcdtest@gmail.com", "is_primary": 1}],
+				"phone_nos": [{"phone": "7897895874", "is_primary_phone": 1, "is_primary_mobile_no": 1}],
+				"links": [{"link_doctype": "Customer", "link_name": customer_name}],
 			}
 		).insert(ignore_permissions=True)
 
