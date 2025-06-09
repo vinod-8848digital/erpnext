b77118a9df1a8ebadf761f55d247bfea69ae99d9
hrishikesh8848 <158465727+hrishikesh8848@users.noreply.github.com>
2025-06-09 07:37:26 +0530

    Update test_delivery_note_trends.py

--- erpnext/stock/report/delivery_note_trends/test_delivery_note_trends.py
+++ erpnext/stock/report/delivery_note_trends/test_delivery_note_trends.py
@@ -30,6 +30,12 @@ class TestDeliveryNoteTrendsReport(unittest.TestCase):
             }]
         }).insert(ignore_permissions=True)
 		self.dn.submit()
+		fiscal_year = frappe.new_doc("Fiscal Year")
+        fiscal_year.year = "2024-2025"
+        fiscal_year.year_start_date = "01-04-2024"
+        fiscal_year.year_end_date = "31-03-2025"
+        fiscal_year.append("companies", {"company": "_Test Company"})
+        fiscal_year.save()
 		
 	def test_execute_with_valid_filters(self):
 		from erpnext.stock.report.delivery_note_trends import delivery_note_trends
