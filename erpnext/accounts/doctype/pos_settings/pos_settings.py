74081c6191d79813a78662ae1aceb08430ac0ee9
Nandhini8848 <nandhini@8848digital.com>
2025-08-11 14:33:59 +0530

    fix: add pragma flag for type checking

--- erpnext/accounts/doctype/pos_settings/pos_settings.py
+++ erpnext/accounts/doctype/pos_settings/pos_settings.py
@@ -11,7 +11,7 @@ class POSSettings(Document):
 
 	from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
+	if TYPE_CHECKING:  # pragma: no cover
 		from frappe.types import DF
 
 		from erpnext.accounts.doctype.pos_field.pos_field import POSField
